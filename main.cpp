5aa201aaa74fcb367581a492f008a11c619892e2
James Beck <james@jmbeck.com>
2013-05-04 14:27:00 +0200

    Added file details for Windows, and updated version number

--- main.cpp
+++ main.cpp
@@ -28,8 +28,9 @@ QString appVersion()
     // 1.2 - fixed the animal life toggling on load
     // 1.3 - added coffee, gave new units coffee
     // 1.4 - updated for T&S 1.1.
+    // 1.5 - fixed oddly-infrequent crash
     //
-    return "1.4 (for T&S v1.1)";
+    return "1.5 (for T&S v1.1)";
 }
 
 int main(int argc, char *argv[])
